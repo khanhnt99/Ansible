e5fa17f2a684f740ccad937e518b95f64413559f
khanhnt99 <ntkhanh99.vn@gmail.com>
2021-06-06 16:37:42 +0700

    Ansible to run Openstack

---
+++ copy-file-netplan.sh
@@ -0,0 +1,5 @@
+scp /root/ansible-netplan/52-cloud-init.yaml root@203.0.0.2:/etc/netplan/
+scp /root/ansible-netplan/53-cloud-init.yaml root@203.0.0.3:/etc/netplan/
+scp /root/ansible-netplan/54-cloud-init.yaml root@203.0.0.4:/etc/netplan/
+scp /root/ansible-netplan/55-cloud-init.yaml root@203.0.0.5:/etc/netplan/
+scp /root/ansible-netplan/56-cloud-init.yaml root@203.0.0.6:/etc/netplan/
---
+++ ssh-copy.sh
@@ -0,0 +1,5 @@
+ssh-copy-id root@203.0.0.2
+ssh-copy-id root@203.0.0.3
+ssh-copy-id root@203.0.0.4
+ssh-copy-id root@203.0.0.5
+ssh-copy-id root@203.0.0.6
